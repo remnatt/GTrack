033fe1f2869ecb99af0313dfa4b94b44cd766521
remnatt <setmind382@gmail.com>
2025-04-21 22:44:37 -0400

    loggetr

---
+++ src/logger_wrapper.pyx
@@ -0,0 +1,6 @@
+# logger_wrapper.pyx
+cdef extern from "logger.h":
+    void log_to_file(const char *filename, const char *action, const char *file, const char *message)
+
+def clog(str filename, str action, str file, str message):
+    log_to_file(filename.encode('utf-8'), action.encode('utf-8'), file.encode('utf-8'), message.encode('utf-8'))
