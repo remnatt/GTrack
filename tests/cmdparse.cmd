28f248a3acb86b34ad37bf7f634443c18d6b663c
remnatt <setmind382@gmail.com>
2025-04-21 22:39:48 -0400

    test parsing

---
+++ tests/cmdparse.cmd
@@ -0,0 +1,6 @@
+python GTrack.py --api --botAs 'GTracker' --incommit --readme=true --bold \
+--licsensetype 1 --gitignore=false --extrafile 'header' \
+--extrafiletype py --content '# this is a header \n import os # this is a import' \
+--customcommitmessage ':green checkmark: test commit' --vmode
+
+  "yet to be confirmed or put back into work."
