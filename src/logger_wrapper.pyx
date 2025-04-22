cbed451deb24cd97b8c3c988f2ec19996e8f7642
remnatt <setmind382@gmail.com>
2025-04-21 23:05:43 -0400

    Update logger_wrapper.pyx

--- src/logger_wrapper.pyx
+++ src/logger_wrapper.pyx
@@ -1,6 +1,7 @@
 # logger_wrapper.pyx
+
 cdef extern from "logger.h":
     void log_to_file(const char *filename, const char *action, const char *file, const char *message, int add_timestamp)
 
-def clog(str filename, str action, str file, str message, bool add_timestamp):
-    log_to_file(filename.encode('utf-8'), action.encode('utf-8'), file.encode('utf-8'), message.encode('utf-8'), int(add_timestamp))
+def clog(str filename, str action, str file, str message, bint add_timestamp):  # <- use `bint`
+    log_to_file(filename.encode('utf-8'), action.encode('utf-8'), file.encode('utf-8'), message.encode('utf-8'), add_timestamp)
