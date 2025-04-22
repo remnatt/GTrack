d1d4c14572dd59b1f5aa2bb505e618092c46fddf
remnatt <setmind382@gmail.com>
2025-04-21 22:43:46 -0400

    Create logger.h

---
+++ src/logger.h
@@ -0,0 +1,9 @@
+// src/logger.h
+#ifndef LOGGER_H
+#define LOGGER_H
+
+void log_to_file(const char *filename, const char *action, const char *file, const char *message);
+
+#endif
+// GTrack
+// To be used in python via cython
