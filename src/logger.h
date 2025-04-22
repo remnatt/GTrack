90e351689b09d00ca8908078ea42770576591194
remnatt <setmind382@gmail.com>
2025-04-21 22:52:26 -0400

    logger

--- src/logger.h
+++ src/logger.h
@@ -2,8 +2,6 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-void log_to_file(const char *filename, const char *action, const char *file, const char *message);
+void log_to_file(const char *filename, const char *action, const char *file, const char *message, int add_timestamp);
 
 #endif
-// GTrack
-// To be used in python via cython
