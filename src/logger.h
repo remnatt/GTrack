d0e65f4d0101c3755ba8184249c844f9570eb1e1
remnatt <setmind382@gmail.com>
2025-04-21 22:52:40 -0400

    logger

--- src/logger.h
+++ src/logger.h
@@ -1,7 +1,20 @@
-// src/logger.h
-#ifndef LOGGER_H
-#define LOGGER_H
+// src/logger.c
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
-void log_to_file(const char *filename, const char *action, const char *file, const char *message, int add_timestamp);
+void log_to_file(const char *filename, const char *action, const char *file, const char *message, int add_timestamp) {
+    FILE *logfile = fopen(filename, "a");
+    if (!logfile) return;
 
-#endif
+    if (add_timestamp) {
+        time_t now = time(NULL);
+        char *timestamp = ctime(&now);
+        if (timestamp) timestamp[strcspn(timestamp, "\n")] = 0;
+        fprintf(logfile, "[%s] ", timestamp);
+    }
+
+    fprintf(logfile, "%s | %s | %s\n", action, file, message);
+    fclose(logfile);
+}
