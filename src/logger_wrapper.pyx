cd5dc46d0c9a7ed44cb7a7abc810299bb89c2ffb
remnatt <setmind382@gmail.com>
2025-04-21 22:52:10 -0400

    logger

--- src/logger_wrapper.pyx
+++ src/logger_wrapper.pyx
@@ -1,6 +1,6 @@
 # logger_wrapper.pyx
 cdef extern from "logger.h":
-    void log_to_file(const char *filename, const char *action, const char *file, const char *message)
+    void log_to_file(const char *filename, const char *action, const char *file, const char *message, int add_timestamp)
 
-def clog(str filename, str action, str file, str message):
-    log_to_file(filename.encode('utf-8'), action.encode('utf-8'), file.encode('utf-8'), message.encode('utf-8'))
+def clog(str filename, str action, str file, str message, bool add_timestamp):
+    log_to_file(filename.encode('utf-8'), action.encode('utf-8'), file.encode('utf-8'), message.encode('utf-8'), int(add_timestamp))
